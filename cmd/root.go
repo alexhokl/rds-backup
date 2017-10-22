a8ebb918ede0a4626337ae07cd30eaa678ee3de9
alexhokl <alexhokl@gmail.com>
2017-10-22 12:45:13 +0800

    Added prefix for environment variables

--- cmd/root.go
+++ cmd/root.go
@@ -72,6 +72,8 @@ func initConfig() {
 		viper.SetConfigName(".rds-backup")
 	}
 
+	viper.SetEnvPrefix("RDS_BACKUP_")
+
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
